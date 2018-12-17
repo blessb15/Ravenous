70b1e740b567b334d6014a3b8be6bbf1dcf545b2
blessb15 <blakebless7@gmail.com>
2018-12-17 15:07:31 -0800

    add components needed for site

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'SearchBar.css';
+
+const sortByOptions = {
+  BestMatch: this,
+  HighestRated: this.rating,
+  MostReviewed: this.reviewCount
+}
+
+class SearchBar extends React.Component {
+  renderSortByOptions(){
+
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Business from './src/components/Business';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Business />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
